fa9554f1991d67e80ff120bde601d14a1e8f3e10
AustinOngwae <austnya@outlook.com>
2022-03-20 15:56:12 +0300

    Connected Admin Nav Bar to all the pages

--- carwash/admin/services.php
+++ carwash/admin/services.php
@@ -13,9 +13,9 @@ if(!isset($_SESSION["username"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Top Touch Car Wash</title>
+    <title>Services - Top Touch Car Wash</title>
     <link rel="stylesheet" type="text/css" href="../resources/css/header.css">
-    <link rel="stylesheet" type="text/css" href="../resources/css/admin/home.css">
+    <link rel="stylesheet" type="text/css" href="../resources/css/admin/services.css">
 </head>
 
 <body>
