1849f01610817a48b7b2699ec0a5326c920e6d57
AustinOngwae <austnya@outlook.com>
2022-03-28 14:35:39 +0300

    20 % Progress on billing foem

--- carwash/user/billing.php
+++ carwash/user/billing.php
@@ -21,41 +21,112 @@ if(!isset($_SESSION["username"])){
 
     <?php
     include "../userheader.php";
-    
+    if(isset($_POST['proceed'])) {
+        $total = 0;
+
+        if(isset($_POST['service_1'])) {
+            $service1 = $_POST['service_1'];
+            $total +=$service1;
+        }
+        if(isset($_POST['service_2'])) {
+            $service2 = $_POST['service_2'];
+            $total +=$service2;
+        }
+        if(isset($_POST['service_3'])) {
+            $service3 = $_POST['service_3'];
+            $total +=$service3;
+        }
+        if(isset($_POST['service_4'])) {
+            $service4 = $_POST['service_4'];
+            $total +=$service4;
+        }
+        if(isset($_POST['service_5'])) {
+            $service5 = $_POST['service_5'];
+            $total +=$service5;
+        }
+        if(isset($_POST['service_6'])) {
+            $service6 = $_POST['service_6'];
+            $total +=$service6;
+        }
+        if(isset($_POST['service_7'])) {
+            $service7 = $_POST['service_7'];
+            $total +=$service7;
+        }
+        if(isset($_POST['service_8'])) {
+            $service8 = $_POST['service_8'];
+            $total +=$service8;
+        }
+        if(isset($_POST['service_9'])) {
+            $service9 = $_POST['service_9'];
+            $service9 = intval( $service9 );
+            $total +=$service9;
+        }
+        if(isset($_POST['service_10'])) {
+            $service10 = $_POST['service_10'];
+            $service10 = intval( $service10 );
+            $total +=$service10;
+        }
+        
+        $_SESSION['total'] = $total;
+
+    }
     ?>
 
     <main>
+        <?php if(isset($_SESSION['total'])){
+            echo "<p class='invoice-title'>Invoice:</p>";
+            echo "<table class='invoice-table'>";
+                echo "<tr>" .
+                "<th>Bill Details</th>" .
+                "<th>Services Provided</th>" .
+                "</tr>";
+                echo "<tr>" .
+                "<td>" . "Total Bill: " . $_SESSION['total'] . "</td>" .
+                "<td>" . "service sdfsldfsjdfsdfsdflsdjfsdljfsdljsdjfljflsjdfjflsjfskdjfo" . "</td>" .
+                "</tr>" . 
+                "<tr>" . 
+                "<td>Commission: 50/=</td>" .
+                "</tr>";
+            echo "</table>";
+
+            //ECHO A FORM WITH HIDDEN INPUTS THAT WILL ONLY HAVE THE BUTTON FOR SUBMITTING VISSIBLE IN ORDER TO POST THE DATA 
+
+
+            // echo "<p class='alert-success'>" . $_SESSION['total'] . "</p>";
+            unset($_SESSION['total']);
+        } 
+        ?>
         <div class="register">
                 <div class="title">Bill Customer</div>
                 <form action="" method="POST" class="form">
                     <div class="inputfield">
                         <label>Date</label>
-                        <input type="date" class="input" name="dateAdded" placeholder="dd/mm/yyyy" required/>
+                        <input type="date" class="input" name="dateAdded" placeholder="dd/mm/yyyy" />
                     </div>
                     <div class="inputfield">
                         <label>Employee National Id</label>
-                        <input type="number" class="input" name="phoneNo"  placeholder="Employee National Id" required/>
+                        <input type="number" class="input" name="phoneNo"  placeholder="Employee National Id" />
                     </div>    
                 <div class="sub-title">Enter Customer Details</div>
                     <div class="inputfield">
                         <label>Plate No</label>
-                        <input type="text" class="input" name="plateNo"  placeholder="Plate No" required/>
+                        <input type="text" class="input" name="plateNo"  placeholder="Plate No" />
                     </div>
                     <div class="inputfield">
                         <label>Model</label>
-                        <input type="text" class="input" name="model"  placeholder="Model" required/>
+                        <input type="text" class="input" name="model"  placeholder="Model" />
                     </div>
                     <div class="inputfield">
                         <label>colour</label>
-                        <input type="text" class="input" name="colour"  placeholder="Colour" required/>
+                        <input type="text" class="input" name="colour"  placeholder="Colour" />
                     </div>
                     <div class="inputfield">
                         <label>Owner Name</label>
-                        <input type="text" class="input" name="ownerName"  placeholder="Owner's Name" required/>
+                        <input type="text" class="input" name="ownerName"  placeholder="Owner's Name" />
                     </div>
                     <div class="inputfield">
                         <label>Phone No</label>
-                        <input type="number" class="input" name="phoneNo"  placeholder="Phone No" required/>
+                        <input type="number" class="input" name="phoneNo"  placeholder="Phone No" />
                     </div>
                     <div class="sub-title">Select Services</div>
                     <div class="inputfield-chk">
@@ -74,7 +145,7 @@ if(!isset($_SESSION["username"])){
                             $row = $result->fetch_assoc();
                         ?>
                         <input type="checkbox" class="input-chk" name="service_2" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <label for="service_2"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="inputfield-chk">
                         <?php 
@@ -83,7 +154,7 @@ if(!isset($_SESSION["username"])){
                             $row = $result->fetch_assoc();
                         ?>
                         <input type="checkbox" class="input-chk" name="service_3" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <label for="service_3"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="inputfield-chk">
                         <?php 
@@ -92,7 +163,7 @@ if(!isset($_SESSION["username"])){
                             $row = $result->fetch_assoc();
                         ?>
                         <input type="checkbox" class="input-chk" name="service_4" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <label for="service_4"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="inputfield-chk">
                         <?php 
@@ -100,8 +171,8 @@ if(!isset($_SESSION["username"])){
                             $result = $connect->query($sql);
                             $row = $result->fetch_assoc();
                         ?>
-                        <input type="checkbox" class="input-chk" name="service_4" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <input type="checkbox" class="input-chk" name="service_5" value="<?php echo $row['cost'] ?>"/>
+                        <label for="service_5"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="inputfield-chk">
                         <?php 
@@ -110,7 +181,7 @@ if(!isset($_SESSION["username"])){
                             $row = $result->fetch_assoc();
                         ?>
                         <input type="checkbox" class="input-chk" name="service_6" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <label for="service_6"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="inputfield-chk">
                         <?php 
@@ -119,7 +190,7 @@ if(!isset($_SESSION["username"])){
                             $row = $result->fetch_assoc();
                         ?>
                         <input type="checkbox" class="input-chk" name="service_7" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <label for="service_7"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="inputfield-chk">
                         <?php 
@@ -128,7 +199,7 @@ if(!isset($_SESSION["username"])){
                             $row = $result->fetch_assoc();
                         ?>
                         <input type="checkbox" class="input-chk" name="service_8" value="<?php echo $row['cost'] ?>"/>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
+                        <label for="service_8"><?php echo $row["name"] . " - " . $row['description'] . " @" . $row['cost'] . "/=" ?></label>
                     </div>
                     <div class="sub-title-special">Some services require manual cost entry when billing:</div>
                     <div class="inputfield-2">
@@ -137,7 +208,7 @@ if(!isset($_SESSION["username"])){
                             $result = $connect->query($sql);
                             $row = $result->fetch_assoc();
                         ?>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description']?></label>
+                        <label for="service_9"><?php echo $row["name"] . " - " . $row['description']?></label>
                         <input type="number" class="input" name="service_9"  placeholder="Enter the service amount"/>
                     </div>
                     <div class="inputfield-2">
@@ -146,7 +217,7 @@ if(!isset($_SESSION["username"])){
                             $result = $connect->query($sql);
                             $row = $result->fetch_assoc();
                         ?>
-                        <label for="service_1"><?php echo $row["name"] . " - " . $row['description']?></label>
+                        <label for="service_10"><?php echo $row["name"] . " - " . $row['description']?></label>
                         <input type="number" class="input" name="service_10"  placeholder="Enter the service amount"/>
                     </div>
                     
