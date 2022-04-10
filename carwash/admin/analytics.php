f726232d0be341a97a5cf75759b7aab80b70f180
AustinOngwae <austnya@outlook.com>
2022-04-10 13:40:57 +0300

    Put dummy charts

--- carwash/admin/analytics.php
+++ carwash/admin/analytics.php
@@ -15,6 +15,53 @@ if(!isset($_SESSION["username"])) {
     <title>Analytics - Top Touch Car Wash</title>
     <link rel="stylesheet" type="text/css" href="../resources/css/header.css">
     <link rel="stylesheet" type="text/css" href="../resources/css/admin/analytics.css">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      google.charts.setOnLoadCallback(drawChart2);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
+      function drawChart2() {
+
+        var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7]
+        ]);
+
+        var options = {
+        title: 'My Daily Activities 2'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+
+        chart.draw(data, options);
+        }
+    </script>
 </head>
 
 <body>
@@ -23,7 +70,11 @@ if(!isset($_SESSION["username"])) {
     
     ?>
     <main class="main-container">
-        
+        <div class="main-container">
+        <div id="piechart" style="width: 900px; height: 500px;"></div>
+        <div id="piechart2" style="width: 900px; height: 500px;"></div>
+        </div>
+    
     </main>
 </body>
 
